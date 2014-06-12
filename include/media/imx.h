f1dea5eb74bd4d1f6657ac262673d7d74c6a6cac
Philipp Zabel <p.zabel@pengutronix.de>
2014-06-12 19:06:28 +0200

    Add i.MX SoC wide media device driver
    This driver registers a single, SoC wide media device, which all entities
    in the media graph can be registered with via OF graph bindings.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

---
+++ include/media/imx.h
@@ -0,0 +1,25 @@
+struct v4l2_subdev;
+struct device_node;
+struct ipu_media_link;
+struct v4l2_device;
+struct media_device;
+struct device;
+
+struct ipu_media_link *ipu_media_entity_create_link(struct v4l2_subdev *sd,
+		int padno, struct device_node *remote_node,
+		u32 media_link_flags);
+
+void ipu_media_entity_remove_link(struct ipu_media_link *link);
+
+struct v4l2_device *ipu_media_get_v4l2_dev(void);
+
+struct media_device *ipu_find_media_device(void);
+
+#ifdef CONFIG_MEDIA_IMX
+int ipu_media_device_register(struct device *dev);
+#else
+static inline int ipu_media_device_register(struct device *dev)
+{
+	return 0;
+}
+#endif
