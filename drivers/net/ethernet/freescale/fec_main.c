e8e09ea9f8f6be17ed2aa18c4e67440318a3af41
Christian Hemp <c.hemp@phytec.de>
2016-09-29 12:08:11 +0200

    net: fec: Use alias for device number
    Use the devicetree alias to set the ethernet device number.
    Otherwise the second ethernet port has the name eth0 and the first has
    the name eth1.
    
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Signed-off-by: Christoph Fritz <chf@fritzc.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3332,6 +3332,7 @@ fec_probe(struct platform_device *pdev)
 	struct device_node *np = pdev->dev.of_node, *phy_node;
 	int num_tx_qs;
 	int num_rx_qs;
+	int eth_id;
 
 	fec_enet_get_queue_num(pdev, &num_tx_qs, &num_rx_qs);
 
@@ -3511,6 +3512,11 @@ fec_probe(struct platform_device *pdev)
 	netif_carrier_off(ndev);
 	fec_enet_clk_enable(ndev, false);
 	pinctrl_pm_select_sleep_state(&pdev->dev);
+
+	eth_id = of_alias_get_id(pdev->dev.of_node, "ethernet");
+
+	if (eth_id >= 0)
+		sprintf(ndev->name, "eth%d", eth_id);
 
 	ret = register_netdev(ndev);
 	if (ret)
