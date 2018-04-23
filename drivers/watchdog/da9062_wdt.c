93fa3457f90f49bad48b3d0c9bec809e161a3c60
Stefan Riedmueller <s.riedmueller@phytec.de>
2018-04-23 17:02:41 +0200

    watchdog: da9062: Check for running watchdog during probe
    The watchdog might have been started in the bootloader to catch boot
    failures. In this case keep the watchdog running with the default
    timeout.
    
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>

--- drivers/watchdog/da9062_wdt.c
+++ drivers/watchdog/da9062_wdt.c
@@ -183,6 +183,14 @@ static int da9062_wdt_restart(struct watchdog_device *wdd, unsigned long action,
 	return ret;
 }
 
+static inline bool da9062_wdt_is_running(struct da9062_watchdog *wdt)
+{
+	int val;
+
+	regmap_read(wdt->hw->regmap, DA9062AA_CONTROL_D, &val);
+	return val & DA9062AA_TWDSCALE_MASK;
+}
+
 static const struct watchdog_info da9062_watchdog_info = {
 	.options = WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING,
 	.identity = "DA9062 WDT",
@@ -233,6 +241,17 @@ static int da9062_wdt_probe(struct platform_device *pdev)
 
 	watchdog_set_drvdata(&wdt->wdtdev, wdt);
 
+	if (da9062_wdt_is_running(wdt)) {
+		ret = da9062_wdt_set_timeout(&wdt->wdtdev, wdt->wdtdev.timeout);
+		if (ret < 0)
+			return ret;
+
+		set_bit(WDOG_HW_RUNNING, &wdt->wdtdev.status);
+		dev_info(wdt->hw->dev,
+			 "watchdog is running, setting default timeout: %u sec\n",
+			 wdt->wdtdev.timeout);
+	}
+
 	ret = devm_watchdog_register_device(&pdev->dev, &wdt->wdtdev);
 	if (ret < 0) {
 		dev_err(wdt->hw->dev,
@@ -240,7 +259,7 @@ static int da9062_wdt_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	return da9062_wdt_ping(&wdt->wdtdev);
+	return 0;
 }
 
 static struct platform_driver da9062_wdt_driver = {
