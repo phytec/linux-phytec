dc73d25ba9f2c6a0aae454428f977b2e8a542b65
Stefan Riedmueller <s.riedmueller@phytec.de>
2018-04-27 10:29:02 +0200

    regulator: core: Add regulator_set_bypass function
    This patch introduces a new regulator function which can set or unset
    the bypass mode of a regulator although not all consumers actively allow
    bypass mode.
    
    To bypass a regulator a valid supply needs to be present. So check for a
    supply and make sure it is not a dummy before setting bypass.
    
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -263,6 +263,7 @@ int regulator_get_error_flags(struct regulator *regulator,
 int regulator_set_load(struct regulator *regulator, int load_uA);
 
 int regulator_allow_bypass(struct regulator *regulator, bool allow);
+int regulator_set_bypass(struct regulator *regulator, bool allow);
 
 struct regmap *regulator_get_regmap(struct regulator *regulator);
 int regulator_get_hardware_vsel_register(struct regulator *regulator,
@@ -512,6 +513,12 @@ static inline int regulator_set_load(struct regulator *regulator, int load_uA)
 }
 
 static inline int regulator_allow_bypass(struct regulator *regulator,
+					 bool allow)
+{
+	return 0;
+}
+
+static inline int regulator_set_bypass(struct regulator *regulator,
 					 bool allow)
 {
 	return 0;
