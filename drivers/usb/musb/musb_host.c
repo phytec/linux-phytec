55b9e67b9513b6874ab107dd5a23671770f2a7eb
Bin Liu <b-liu@ti.com>
2018-05-21 08:42:19 -0500

    UPSTREAM: usb: musb: disable otg protocol support
    As decided in the discussion [1] we are deleting the otg protocol
    support from the musb drivers.
    
    First this patch disables the flags for enabling the otg protocols. We
    will later gradually delete the otg protocol code from the musb drivers.
    
    [1] https://www.spinics.net/lists/linux-usb/msg167003.html
    
    Signed-off-by: Bin Liu <b-liu@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    With the 4.14er kernel the host mode of OTG is only working after suspend/resume.
    I have looked for a fix but found that upstream is disabling the support of
    usb otg. So the usb otg will now only function as device. As this will be disabled
    upstream anyway I will cherry-pick the patch from upstream.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -2792,7 +2792,8 @@ int musb_host_setup(struct musb *musb, int power_budget)
 		musb->xceiv->otg->state = OTG_STATE_A_IDLE;
 	}
 	otg_set_host(musb->xceiv->otg, &hcd->self);
-	hcd->self.otg_port = 1;
+	/* don't support otg protocols */
+	hcd->self.otg_port = 0;
 	musb->xceiv->otg->host = &hcd->self;
 	hcd->power_budget = 2 * (power_budget ? : 250);
 
