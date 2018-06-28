a1db9d0f5783909304546471049ebdc94088e4a7
Stefan Christ <s.christ@phytec.de>
2018-06-28 09:53:33 +0200

    mfd: da9063: disable timeout feature
    It fixes various hangs of the i.MX6 module, when, for example, hackbench is
    running in a loop. This is a port of previous patch onto 3.19.5:
    
        commit de7974db320349954d3a0479e3d6eb2c7b60dc1b
        Author: Christian Hemp <c.hemp@phytec.de>
        Date:   Thu Jan 9 10:54:37 2014 +0100
    
            mfd: da9063: disable timeout feature
    
            This patch fix bug report #131: i.MX6 crashes: voltage too low
                | Sometimes, after voltage scaling, the processor hangs. It seems, that the
                | voltage is set too low for appropriate frequency setting.
                |
                | It seems that the problem occurs only if the delay between two message bits
                | (e.g. between Send Slave address write command and send data command) is
                | greater than 23ms. If this delay occurs, the PMIC seems to reset the I2C
                | interface and so doesn't react to the other bits of the message (e.g. by
                | not sending a ACK).
    
            If we deactivate the I2C timeout feature the module runs stable.
    
            Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    
    Signed-off-by: Stefan Christ <s.christ@phytec.de>
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Conflicts:
    	drivers/mfd/da9063-core.c
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>
    
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>

--- drivers/mfd/da9063-core.c
+++ drivers/mfd/da9063-core.c
@@ -166,7 +166,7 @@ static int da9063_clear_fault_log(struct da9063 *da9063)
 int da9063_device_init(struct da9063 *da9063, unsigned int irq)
 {
 	struct da9063_pdata *pdata = da9063->dev->platform_data;
-	int model, variant_id, variant_code;
+	int model, variant_id, variant_code, config_j;
 	int ret;
 
 	ret = da9063_clear_fault_log(da9063);
@@ -246,6 +246,22 @@ int da9063_device_init(struct da9063 *da9063, unsigned int irq)
 			dev_err(da9063->dev, "Failed to add child devices\n");
 			return ret;
 		}
+	}
+
+	/* Set bit 6 (IF_TO) of register 0x10F (DA9063_REG_CONFIG_J) to 0:
+	 *    Disables automatic reset of 2-WIRE-IF
+	 */
+	ret = regmap_read(da9063->regmap, DA9063_REG_CONFIG_J, &config_j);
+	if (ret) {
+		dev_err(da9063->dev, "Cannot read register DA9063_REG_CONFIG_J.\n");
+		return ret;
+	}
+
+	config_j &= ~0x40;
+	ret = regmap_write(da9063->regmap, DA9063_REG_CONFIG_J, config_j);
+	if (ret) {
+		dev_err(da9063->dev, "Cannot write register DA9063_REG_CONFIG_J.\n");
+		return ret;
 	}
 
 	return ret;
