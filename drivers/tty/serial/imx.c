b949b01e8c3fd281b02048ab13ddd89e145938d1
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2018-11-29 16:16:08 +0100

    BACKPORT: serial: imx: Stop to receive in .stop_rx()
    When the UART is used in DMA mode, .stop_rx() does nothing if the port
    isn't suspended. This is wrong as .stop_rx() should stop receiving
    characters unconditionally. When the port is about to be closed the DMA
    channel is stopped in .shutdown(), so this isn't necessary to be in
    .stop_rx() here, too.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -424,15 +424,6 @@ static void imx_stop_rx(struct uart_port *port)
 	struct imx_port *sport = (struct imx_port *)port;
 	unsigned long ucr1, ucr2;
 
-	if (sport->dma_is_enabled && sport->dma_is_rxing) {
-		if (sport->port.suspended) {
-			dmaengine_terminate_all(sport->dma_chan_rx);
-			sport->dma_is_rxing = 0;
-		} else {
-			return;
-		}
-	}
-
 	ucr1 = readl(sport->port.membase + UCR1);
 	ucr2 = readl(sport->port.membase + UCR2);
 
