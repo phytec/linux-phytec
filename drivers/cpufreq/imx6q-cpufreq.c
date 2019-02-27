6b8a80e8539293dff40d464c64a5b678098bda01
Bai Ping <b51503@freescale.com>
2019-02-27 15:15:40 +0100

    NXP: MLK-11252-02 cpufreq: imx: add dc regulator in cpufreq driver
    In some i.MX6 board (i.MX6UL EVK), it will have a additional
    GPIO controlled DC-DC regulator. we need to change this regulator's
    voltage According the maximum frequency that CPU can run at.
    
    At present, we only need to set the voltage to minimum one, we need
    to set the voltage to the maximum before suspend, resume back to the
    minimum voltage after suspend.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Conflict: drivers/cpufreq/imx6q-cpufreq.c
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>

--- drivers/cpufreq/imx6q-cpufreq.c
+++ drivers/cpufreq/imx6q-cpufreq.c
@@ -20,6 +20,8 @@
 
 #define PU_SOC_VOLTAGE_NORMAL	1250000
 #define PU_SOC_VOLTAGE_HIGH	1275000
+#define DC_VOLTAGE_MIN		1300000
+#define DC_VOLTAGE_MAX		1400000
 #define FREQ_1P2_GHZ		1200000000
 #define FREQ_800_MHZ            792000000
 #define FREQ_400_MHZ            396000000
@@ -27,6 +29,7 @@
 static struct regulator *arm_reg;
 static struct regulator *pu_reg;
 static struct regulator *soc_reg;
+static struct regulator *dc_reg;
 
 enum IMX6_CPUFREQ_CLKS {
 	ARM,
@@ -362,6 +365,9 @@ static int imx6q_cpufreq_suspend(struct cpufreq_policy *policy)
 {
 	int ret;
 
+	if (!IS_ERR(dc_reg))
+		regulator_set_voltage_tol(dc_reg, DC_VOLTAGE_MAX, 0);
+
 	if (bypass) {
 		ret = imx6q_cpufreq_prepare_bypass(false);
 		if (ret) {
@@ -390,6 +396,9 @@ static int imx6q_cpufreq_suspend(struct cpufreq_policy *policy)
 static int imx6q_cpufreq_resume(struct cpufreq_policy *policy)
 {
 	int ret;
+
+	if (!IS_ERR(dc_reg))
+		regulator_set_voltage_tol(dc_reg, DC_VOLTAGE_MIN, 0);
 
 	if (bypass) {
 		ret = imx6q_cpufreq_prepare_bypass(true);
@@ -589,6 +598,10 @@ static int imx6q_cpufreq_probe(struct platform_device *pdev)
 		ret = -ENOENT;
 		goto put_reg;
 	}
+
+	dc_reg = devm_regulator_get_optional(cpu_dev, "dc");
+	if (!IS_ERR(dc_reg))
+		regulator_set_voltage_tol(dc_reg, DC_VOLTAGE_MIN, 0);
 
 	ret = dev_pm_opp_of_add_table(cpu_dev);
 	if (ret < 0) {
