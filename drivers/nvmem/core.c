c5cdcd8ae00f7dcb5b5129988fd97f7edbd212a3
Ahmad Fatoum <a.fatoum@pengutronix.de>
2020-09-14 11:13:14 +0200

    BSPYOCTOIMX6-1030 FROMLIST: HACK: nvmem: core: don't consider subnodes not matching binding
    The nvmem cell binding applies to objects which match "^.*@[0-9a-f]+$",
    but so far the driver has matched all objects and failed if they didn't
    have the expected properties.
    
    The driver's behavior in this regard precludes future extension of
    EEPROMs by child nodes other than nvmem and clashes with the barebox
    bootloader binding that extends the fixed-partitions MTD binding to
    EEPROMs as it tries to interpret the "fixed-partitions"-compatible
    partitions node as a nvmem cell.
    
    Solve this issue by skipping all subnodes that don't contain an @.
    
    This still allows for cell names like `partitions@0,0', but this
    is much less likely to cause future collisions.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    
    This patch fixes the EEPROM failing to initialize:
    ```
    [    3.624361] nvmem 0-00520: nvmem: invalid reg on /soc/aips-bus@2100000/i2c@21a0000/eeprom@52/partitions
    [    3.635835] at24: probe of 0-0052 failed with error -22
    ```
    (am from https://lkml.kernel.org/lkml/d59007d1-f4e3-f550-cecd-64b788a534bc@linaro.org/T/ )
    Signed-off-by: Robert Karszniewicz <r.karszniewicz@phytec.de>
    Reviewed-by: Yunus Bas <y.bas@phytec.de>

--- drivers/nvmem/core.c
+++ drivers/nvmem/core.c
@@ -298,6 +298,8 @@ static int nvmem_add_cells_from_of(struct nvmem_device *nvmem)
 	parent = dev->of_node;
 
 	for_each_child_of_node(parent, child) {
+		if (!strchr(kbasename(child->full_name), '@'))
+			continue;
 		addr = of_get_property(child, "reg", &len);
 		if (!addr || (len < 2 * sizeof(u32))) {
 			dev_err(dev, "nvmem: invalid reg on %pOF\n", child);
