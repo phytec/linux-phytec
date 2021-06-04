b98e4c4862ca1fb3d91e3acf75d08b380092f3b6
Stefan Agner <stefan@agner.ch>
2021-06-04 12:56:42 +0000

    net: fec: get regulator optional
    According to the device tree binding the phy-supply property is
    optional. Use the regulator_get_optional API accordingly. The
    code already handles NULL just fine.
    
    This gets rid of the following warning:
      fec 2188000.ethernet: 2188000.ethernet supply phy not found, using dummy regulator
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Reviewed-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3602,7 +3602,7 @@ fec_probe(struct platform_device *pdev)
 	if (ret)
 		goto failed_clk_ahb;
 
-	fep->reg_phy = devm_regulator_get(&pdev->dev, "phy");
+	fep->reg_phy = devm_regulator_get_optional(&pdev->dev, "phy");
 	if (!IS_ERR(fep->reg_phy)) {
 		ret = regulator_enable(fep->reg_phy);
 		if (ret) {
