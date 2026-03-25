99a59165befaf72e4d6c4cfa78ade678c3395c97
Christian Hemp <c.hemp@phytec.de>
2026-03-25 17:19:14 +0100

    drm/panel: simple: Set bus format to RGB666
    The phyBOARD-Segin use only 18bit of the LCD interface. To ensure the
    display works correct, the bus_format must be set to
    MEDIA_BUS_FMT_RGB666_1X18.
    
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Signed-off-by: Stefan Riedmüller <s.riedmueller@phytec.de>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -3336,7 +3336,7 @@ static const struct panel_desc powertip_ph800480t032_zhc19 = {
 		.width = 152,
 		.height = 91,
 	},
-	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
+	.bus_format = MEDIA_BUS_FMT_RGB666_1X18,
 	.bus_flags = DRM_BUS_FLAG_DE_HIGH |
 		DRM_BUS_FLAG_PIXDATA_SAMPLE_NEGEDGE |
 		DRM_BUS_FLAG_SYNC_SAMPLE_NEGEDGE,
